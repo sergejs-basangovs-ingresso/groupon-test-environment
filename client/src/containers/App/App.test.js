31dc3407fa5e919b15761ae2ad2025cd95fa0d6d
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-08-23 14:12:39 +0100

    added test ID attr to main app component

--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import { BrowserRouter } from "react-router-dom";
 import { StoreProvider } from "../../context/store";
+import { testAttributes } from "../../data/page.content";
+
 //components:
 import App from "./App.component";
+import { App as AppIsolated } from "./App.component";
 import Header from "../Header/Header.component";
 import LoginPage from "../LoginPage/LoginPage.component";
 
@@ -22,6 +25,14 @@ describe("<App/>", () => {
 
 	it("should render the component", () => {
 		expect(wrapper.containsMatchingElement(<App />)).toEqual(true);
+	});
+
+	it("should contain the data test ID", () => {
+		const shallowWrapper = shallow(<AppIsolated />);
+
+		expect(
+			shallowWrapper.find("[data-testid]").props()["data-testid"]
+		).toEqual(testAttributes.app);
 	});
 
 	it("should render the Header component", () => {
